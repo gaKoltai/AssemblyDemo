97527d10d1df750aa5faa41b597b5383085cf7b2
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-18 12:39:31 +0100

    Added new project to encode and decode a string containing special characters.

---
+++ DynamicCodeDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System;
+
+namespace DynamicCodeDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Web.dll";
+            var webAssembly = Assembly.LoadFrom(path);
+            var utilType = webAssembly.GetType("System.Web.HttpUtility");
+
+            var encode = utilType.GetMethod("HtmlEncode", new Type[]
+            {
+                typeof(string)
+            });
+
+            var decode = utilType.GetMethod("HtmlDecode", new Type[]
+            {
+                typeof(string)
+            });
+
+            const string stringWithSpecialChars = "Marks & Spencer is the shit <sic>";
+            
+             Console.WriteLine(stringWithSpecialChars);
+
+            var encodedString = (string) encode.Invoke(null, new object[]
+            {
+                stringWithSpecialChars
+            });
+            
+            Console.WriteLine(encodedString);
+
+            var decodedString = (string) decode.Invoke(null, new object[]
+            {
+                encodedString
+            });
+
+            Console.WriteLine(decodedString);
+            Console.ReadKey();
+
+        }
+    }
+}
