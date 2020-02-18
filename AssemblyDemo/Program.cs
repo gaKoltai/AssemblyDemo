6a1a86d1d56ebc8ca8d2be8fd55fdc4f818c9a96
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-18 11:20:39 +0100

    Initialized project and created ShowAssemblyInfo function to test if I can access assembly info

---
+++ AssemblyDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.dll";
+
+            Assembly assembly = Assembly.LoadFile(path);
+            ShowAssemblyInfo(assembly);
+
+            Console.ReadKey();
+
+            Assembly ourAssembly = Assembly.GetExecutingAssembly();
+            ShowAssemblyInfo(ourAssembly);
+
+            Console.ReadKey();
+        }
+
+        private static void ShowAssemblyInfo(Assembly assembly)
+        {
+            Console.WriteLine($"The name of the assembly is: {assembly.FullName}");
+            Console.WriteLine($"Is assembly from the global cache? : {assembly.GlobalAssemblyCache}");
+            Console.WriteLine($"The location of the assembly is: {assembly.Location}");
+            Console.WriteLine($"{assembly.ImageRuntimeVersion} is the runtime version of the assembly.");
+
+            foreach (var module in assembly.Modules)
+            {
+                Console.WriteLine($"The name of the the module is: {module.Name}");
+            }
+        }
+    }
+}
