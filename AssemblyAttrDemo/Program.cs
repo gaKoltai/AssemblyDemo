3c00b55b9f924764626168603f226fa96f2cb263
Koltai Gergely Álmos <kgalmos@gmail.com>
2020-02-18 11:50:15 +0100

    Added a new project to display assembly attributes at runtime.

---
+++ AssemblyAttrDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System;
+
+namespace AssemblyAttrDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+            var assemblyAttrType = typeof(AssemblyDescriptionAttribute);
+            var attributes = currentAssembly.GetCustomAttributes(assemblyAttrType, false);
+
+            if (attributes.Length <= 0) return;
+            
+            var descriptionAttr = (AssemblyDescriptionAttribute)attributes[0];
+            Console.WriteLine($"The description of the currently running assembly is: {descriptionAttr.Description}");
+
+        }
+    }
+}
